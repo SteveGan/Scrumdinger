213b4f68f95961bf2d0de202593b4442786272d3
Jialin Gan <stevegan.jialin@gmail.com>
2023-08-07 23:34:16 +0800

    Adding Source of truth and data binding

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,35 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Jialin Gan on 8/5/23.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    @Binding var scrums: [DailyScrum]
+    var body: some View {
+        NavigationStack {
+            List($scrums) { $scrum in
+                NavigationLink(destination: DetailView(scrum: $scrum)) {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar {
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New Scrum")
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: .constant(DailyScrum.sampleData))
+    }
+}
---
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,22 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Jialin Gan on 8/5/23.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self {Self()}
+}
+
